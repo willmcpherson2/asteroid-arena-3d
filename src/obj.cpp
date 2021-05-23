dc82fa9b3203feb78c11e69078109ed95ffc4de1
William McPherson <willmcpherson2@gmail.com>
2021-05-23 21:38:26 +1000

    Rewrite obj::load

--- src/obj.cpp
+++ src/obj.cpp
@@ -1,92 +1,110 @@
 #include "obj.h"
 #include <fstream>
-#include <regex>
-
-static std::sregex_iterator make_it(const std::string& text, const std::regex& re)
-{
-    return std::sregex_iterator(text.begin(), text.end(), re);
-};
-
-static bool it_end(const std::sregex_iterator& it)
-{
-    return it == std::sregex_iterator();
-};
-
-static void get(const std::string& text, const std::regex& re1, const std::regex& re2, const std::function<void(std::sregex_iterator&)>& action)
-{
-    for (auto it1 = make_it(text, re1); !it_end(it1); ++it1) {
-        auto match = it1->str();
-        auto it2 = make_it(match, re2);
-        action(it2);
-    }
-}
+#include <sstream>
+#include <unordered_map>
 
 Model obj::load(const std::string& filename)
 {
+    enum class Keyword {
+        Comment,
+        Vertex,
+        Normal,
+        Face,
+        Smoothing,
+        Material,
+        MaterialLibrary,
+    };
+
+    static std::unordered_map<std::string, Keyword> keywords = {
+        { "#", Keyword::Comment },
+        { "v", Keyword::Vertex },
+        { "vn", Keyword::Normal },
+        { "f", Keyword::Face },
+        { "s", Keyword::Smoothing },
+        { "usemtl", Keyword::Material },
+        { "mtllib", Keyword::MaterialLibrary },
+    };
+
     std::ifstream file(filename, std::ios::in);
-    std::string text(std::istreambuf_iterator<char> { file }, {});
-
-    static std::regex number("-?\\d+\\.\\d+");
-    static std::regex integer("\\d+");
-    static std::regex vertex("v .*");
-    static std::regex normal("vn .*");
-    static std::regex face("f .*");
-
-    std::vector<Vec> vertex_list;
-
-    get(text, vertex, number, [&](auto& it) {
-        auto x = std::stod(it->str());
-        ++it;
-        auto y = std::stod(it->str());
-        ++it;
-        auto z = std::stod(it->str());
-        ++it;
-
-        assert(it_end(it));
-
-        Vec v { x, y, z };
-
-        vertex_list.push_back(v);
-    });
-
-    std::vector<Vec> normal_list;
-
-    get(text, normal, number, [&](auto& it) {
-        auto x = std::stod(it->str());
-        ++it;
-        auto y = std::stod(it->str());
-        ++it;
-        auto z = std::stod(it->str());
-        ++it;
-
-        assert(it_end(it));
-
-        Vec v { x, y, z };
-
-        normal_list.push_back(v);
-    });
 
     Model model;
+    std::vector<Vec> vertex_list;
+    std::vector<Vec> normal_list;
 
-    get(text, face, integer, [&](auto& it) {
-        Polygon p;
+    for (std::string line; std::getline(file, line);) {
+        std::istringstream line_stream(line);
+        std::istream_iterator<std::string> word_it(line_stream);
+        std::istream_iterator<std::string> word_it_end;
 
-        while (!it_end(it)) {
-            size_t vertex_index = std::stoull(it->str()) - 1;
-            assert(vertex_index < vertex_list.size());
-            Vec vertex = vertex_list[vertex_index];
-            ++it;
-
-            size_t normal_index = std::stoull(it->str()) - 1;
-            assert(normal_index < normal_list.size());
-            Vec normal = normal_list[normal_index];
-            ++it;
-
-            p.add({ vertex, normal });
+        Keyword keyword;
+        auto first_word = *word_it;
+        auto key_val_it = keywords.find(first_word);
+        if (key_val_it != keywords.end()) {
+            keyword = key_val_it->second;
+        } else {
+            std::cout << "Unexpected keyword in OBJ file: " << first_word << "\n";
+            assert(false);
         }
+        ++word_it;
 
-        model.add(p);
-    });
+        switch (keyword) {
+        case Keyword::Vertex:
+        case Keyword::Normal: {
+            auto x = std::stod(*word_it);
+            ++word_it;
+            auto y = std::stod(*word_it);
+            ++word_it;
+            auto z = std::stod(*word_it);
+
+            Vec v { x, y, z };
+
+            if (keyword == Keyword::Vertex) {
+                vertex_list.push_back(v);
+            } else {
+                normal_list.push_back(v);
+            }
+
+            break;
+        }
+        case Keyword::Face: {
+            Polygon polygon;
+
+            for (; word_it != word_it_end; ++word_it) {
+                std::istringstream face_element(*word_it);
+
+                std::string vertex_index_str;
+                std::getline(face_element, vertex_index_str, '/');
+                auto vertex_index = std::stoull(vertex_index_str) - 1;
+                assert(vertex_index < vertex_list.size());
+                auto vertex = vertex_list[vertex_index];
+
+                std::string texture_index_str;
+                std::getline(face_element, texture_index_str, '/');
+
+                std::string normal_index_str;
+                std::getline(face_element, normal_index_str, '/');
+                auto normal_index = std::stoull(normal_index_str) - 1;
+                assert(normal_index < normal_list.size());
+                auto normal = normal_list[normal_index];
+
+                polygon.add({ vertex, normal });
+            }
+
+            model.add(polygon);
+
+            break;
+        }
+        case Keyword::Material: {
+            break;
+        }
+        case Keyword::MaterialLibrary: {
+            break;
+        }
+        case Keyword::Comment:
+        case Keyword::Smoothing:
+            break;
+        }
+    }
 
     return model;
 }
